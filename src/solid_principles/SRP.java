3b00688394dabad821bd5399b109b994622092ab
Arvind <arvind231195@gmail.com>
2021-06-27 13:14:35 +0530

    SRP and OCP

---
+++ src/solid_principles/SRP.java
@@ -0,0 +1,78 @@
+package solid_principles;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * SRP:- Single Responsibility Principle
+ *
+ * */
+
+public class SRP {
+    public static void main(String[] args) throws Exception {
+        Journal j = new Journal();
+        j.addEntry("I Cried today");
+        j.addEntry("I ate a bug");
+        System.out.println(j);
+
+        Persistence p = new Persistence();
+        String filename = "C:\\temp\\journal.txt";
+        p.saveToFile(j, filename, true);
+
+        Runtime.getRuntime().exec("notepad.exe " + filename);
+    }
+}
+
+class Journal{
+    private final List<String> entries = new ArrayList<>();
+    private static int count = 0;
+
+    public void addEntry(String text){
+        entries.add(""+ (++count) + ": " + text);
+    }
+
+    public void removeEntry(int index){
+        entries.remove(index);
+    }
+
+    /*
+    * This is persistence. This violates the SRP (journal is to make entries)
+    * not persist and load files. Make a new class regarding it
+    * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+    public void save(String filename) throws FileNotFoundException {
+        try (PrintStream out = new PrintStream(filename)){
+            out.println(toString());
+        }
+    }
+
+    public void load(String filename) { }
+
+    public void load(URL url){ }
+    */
+
+    @Override
+    public String toString() {
+        return String.join(System.lineSeparator(), entries);
+    }
+}
+
+/*
+* Separation of Concerns
+* */
+class Persistence{
+    public void saveToFile(Journal journal, String filename,  boolean overwrite) throws FileNotFoundException{
+        if(overwrite || new File(filename).exists()){
+            try (PrintStream out = new PrintStream(filename)){
+                out.println(journal.toString());
+            }
+        }
+    }
+
+    public Journal loadFromFile(String filename) { return null; }
+    public Journal loadFromFile(URL url) { return null; }
+}
